247a32f100eb6de461b2bb7f126bae8c9a0a458d
JYbmarawcp <fjh2333@gmail.com>
2021-02-25 16:09:41 +0800

    first commit

---
+++ components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './Header.module.css'
+import {Row, Col, Menu, Icon} from 'antd'
+import {HomeOutlined, YoutubeOutlined, SmileOutlined} from '@ant-design/icons'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <Row type="flex" justify="center">
+        <Col xs={24} sm={24} md={10} lg={10} xl={10} className={styles.header_left}>
+          <span className={styles.header_logo}>技术派</span>
+          <span className={styles.header_txt}>专注前端开发，每年100集免费视频</span>
+        </Col>
+        <Col xs={0} sm={0} md={14} lg={8} xl={6}>
+          <Menu mode="horizontal">
+            <Menu.Item key="home">
+              <HomeOutlined />
+              首页
+            </Menu.Item>
+            <Menu.Item key="video">
+              <YoutubeOutlined />
+              视频
+            </Menu.Item>
+            <Menu.Item key="life">
+              <SmileOutlined />
+              生活
+            </Menu.Item>
+          </Menu>
+        </Col>
+      </Row>
+    </div>
+  )
+}
+
+export default Header
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { Button } from 'antd'
+import Header from "../components/Header/Header"
 
 export default function Home() {
   return (
@@ -9,11 +11,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Header></Header>
+{/* 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
+        
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
@@ -50,8 +51,8 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
+      <footer className={styles.footer}> */}
+        {/* <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
@@ -59,7 +60,7 @@ export default function Home() {
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
-      </footer>
+      </footer> */}
     </div>
   )
 }
